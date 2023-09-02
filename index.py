3d09a7eac644f307baebb742c88a7666005da121
Ianã Paranatinga Guerra Fotnteles de Lima <102275095+ianapgf@users.noreply.github.com>
2023-09-01 23:46:57 -0300

    index.py

---
+++ index.py
@@ -0,0 +1,40 @@
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+df = pd.read_csv('sleep.csv')
+
+opcoes = list(df['Occupation'].unique())
+
+app.layout = html.Div(children=[
+    html.H1(children='Qualidade de Sono e Estilo de Vida'),
+
+    html.H2(children='Um olhar adentro de métricas compreensíveis para qualidade de vida'),
+
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': option, 'value': option} for option in opcoes],
+        value=opcoes[0]
+    ),
+
+    dcc.Graph(
+        id='stacked-bar-chart',
+    )
+])
+
+@app.callback(
+    Output('stacked-bar-chart', 'figure'),
+    Input('dropdown', 'value')
+)
+def update_stacked_bar_chart(selected_occupation):
+    filtered_df = df[df['Occupation'] == selected_occupation]
+
+    fig = px.bar(filtered_df, x="Quality of Sleep", y="Physical Activity Level", color="Occupation", barmode="stack",
+                 title=f'Correlação entre Atividade Física e Estresse ({selected_occupation})')
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
